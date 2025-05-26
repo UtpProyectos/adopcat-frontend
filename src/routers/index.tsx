ac7ad3483e5a0c9ae5088e42652a58ef3d52e9b1
RootBackbone <student_cyber_97@protonmail.com>
2025-05-26 03:41:58 -0500

    Update index.tsx

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -5,6 +5,8 @@ import PublicLayout from "../layout/PublicLayout"
 import AdminLayout from "../layout/AdminLayout"
 import Cats from "../pages/global/Cats"
 import CatDetail from "../pages/global/CatDetail"
+import OnlineStore from '../pages/global/OnlineStore';;
+
 
 const AppRouter = () => {
   return (
@@ -18,6 +20,8 @@ const AppRouter = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cats" element={<Cats />} />   
           <Route path="/cats/:id" element={<CatDetail />} />
+          <Route path="/tienda" element={<OnlineStore />} />
+
         </Route>
 
         {/* Layout Admin */}
